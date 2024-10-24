38223d51b07dc10c0d244e3ffcc77154c0b96c77
Tomasz Długoszek <137075040+tdlugoszek@users.noreply.github.com>
2024-10-24 16:30:59 +0200

    Added modulus and power methods and tests to them.

--- src/main/java/org/example/Calculator.java
+++ src/main/java/org/example/Calculator.java
@@ -1,16 +1,30 @@
 package org.example;
 
 public class Calculator {
-    public float add (float number1, float number2) {
-        return number1 + number2;
+    public float add (float a, float b) {
+        return a + b;
     }
 
-    public float deduct (float number1, float number2) {
-        return number1 - number2;
+    public float deduct (float a, float b) {
+        return a - b;
     }
 
-    public float divide (float number1, float number2) {
-        if (number2 == 0) throw new ArithmeticException("/ by zero");
-        return number1 / number2;
+    public float divide (float a, float b) {
+        if (b == 0) throw new ArithmeticException("/ by zero");
+        return a / b;
     }
+
+    public float multiply (float a, float b) {
+        return a * b;
+    }
+
+    public float modulus(int a, int b) {
+        return a % b;
+    }
+
+    public float power(double base, double exponent) {
+        return (float) Math.pow(base, exponent);
+    }
+
+
 }
--- src/test/java/org/example/CalculatorTest.java
+++ src/test/java/org/example/CalculatorTest.java
@@ -3,13 +3,13 @@ package org.example;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
     Calculator calculator  = new Calculator();
-
-
     @Test
     void add() {
         Assertions.assertEquals(2, calculator.add(1,1));
@@ -30,19 +30,20 @@ class CalculatorTest {
         Exception exception = assertThrows(ArithmeticException.class, () ->
                 calculator.divide(1, 0));
         assertEquals("/ by zero", exception.getMessage());
+    }
+    @Test
+    void multiply() {
+        Assertions.assertEquals(10, calculator.multiply(2,5));
+    }
 
-
-
-
-
-//        Assertions.assertEquals(1, calculator.divide(1,0));
+    @Test
+    void modulus() {
+        Assertions.assertEquals(2, calculator.modulus(10,4));
     }
 
 
-
-
-
-
-
-
+    @Test
+    void power() {
+        Assertions.assertEquals(25, calculator.power(5,2));
+    }
 }
