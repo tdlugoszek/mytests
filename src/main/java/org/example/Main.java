4daaf58029212e58dcd6e377c63540d7ade36992
Tomasz Długoszek <137075040+tdlugoszek@users.noreply.github.com>
2024-11-15 01:08:57 +0100

    Refactoring

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,7 +2,5 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.divide(5,0));
     }
 }
