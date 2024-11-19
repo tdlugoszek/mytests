e198193b7de3adf3a4cde20fd0910f06096eda7b
Tomasz Długoszek <137075040+tdlugoszek@users.noreply.github.com>
2024-11-19 01:19:48 +0100

    Add Geometry and GeometryTest

--- src/main/java/org/example/home/Geometry.java
+++ src/main/java/org/example/home/Geometry.java
@@ -12,72 +12,75 @@ import org.example.school.NegativeUnitException;
 
 public class Geometry {
 
-    private void validateInput(double value) {
-        if (value < 0) {
-            throw new NegativeUnitException("Negative values are not allowed: " + value);
+    public double areaOfaRectangle(double a, double b) {
+        if (a <= 0 || b <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a + ", " + b);
         }
-    }
-
-    public double AreaOfaRectangle(double a, double b) {
-        validateInput(a);
-        validateInput(b);
         return a*b;
     }
 
-    public double PerimeterOfaRectangle(double a, double b) {
-        validateInput(a);
-        validateInput(b);
+    public double perimeterOfaRectangle(double a, double b) {
+        if (a <= 0 || b <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a + ", " + b);
+        }
         return 2*(a+b);
     }
 
-    public double AreaOfaTriangle(double a, double h) {
-        validateInput(a);
-        validateInput(h);
+    public double areaOfaTriangle(double a, double h) {
+        if (a <= 0 || h <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a + ",  " + h);
+        }
         return 0.5*a*h;
     }
 
-    public double PerimeterOfaTriangle(double a, double b, double c) {
-        validateInput(a);
-        validateInput(b);
-        validateInput(c);
+    public double perimeterOfaTriangle(double a, double b, double c) {
+        if (a <= 0 || b <= 0 || c <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a + ", " + b + ", " + c);
+        }
         return a + b + c;
     }
 
 
-
-    public double AreaOfaCircle(double r) {
-        validateInput(r);
+    public double areaOfaCircle(double r) {
+        if (r <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + r);
+        }
         return Math.PI*r*r;
     }
 
-    public double PerimeterOfaCircle(double r) {
-        validateInput(r);
+    public double perimeterOfaCircle(double r) {
+        if (r <= 0) {
+            throw new IllegalArgumentException("Radius must be positive: " + r);
+        }
         return 2*Math.PI*r;
     }
 
-    public double AreaOfaTrapeze(double a, double b, double h) {
-        validateInput(a);
-        validateInput(b);
-        validateInput(h);
+    public double areaOfaTrapeze(double a, double b, double h) {
+        if (a <= 0 || b <= 0 || h <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a + ", " + b + ", " + h);
+        }
         return 0.5*(a+b)*h;
     }
 
-    public double PerimeterOfaTrapeze(double a, double b, double c, double d) {
-        validateInput(a);
-        validateInput(b);
-        validateInput(c);
-        validateInput(d);
+    public double perimeterOfaTrapeze(double a, double b, double c, double d) {
+        if (a <= 0 || b <= 0 || c <= 0 || d <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a + ", " + b + ", " + c + ", " + d);
+        }
         return a+b+c+d;
     }
 
 
-    public double AreaOfaSquare(double a) {
-        validateInput(a);
+    public double areaOfaSquare(double a) {
+        if (a <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a);
+        }
         return a*a;
     }
 
-    public double PerimeterOfaSquare(double a) {
-        validateInput(a);
+    public double perimeterOfaSquare(double a) {
+        if (a <= 0) {
+            throw new IllegalArgumentException("Side of the figure must be positive: " + a);
+        }
         return 4*a;
     }
 }
--- src/test/java/org/example/home/GeometryTest.java
+++ src/test/java/org/example/home/GeometryTest.java
@@ -1,48 +1,95 @@
 package org.example.home;
 
+import org.example.school.NegativeUnitException;
 import org.junit.jupiter.api.Test;
 
-
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GeometryTest {
+    Geometry geometry = new Geometry();
 
     @Test
     void areaOfaRectangle() {
+        assertEquals(4, geometry.areaOfaRectangle(2, 2));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaRectangle(1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaRectangle(-1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaRectangle(-1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaRectangle(0, 0));
     }
 
     @Test
     void perimeterOfaRectangle() {
+        assertEquals(4, geometry.perimeterOfaRectangle(1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaRectangle(1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaRectangle(-1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaRectangle(-1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaRectangle(0, 0));
     }
 
     @Test
     void areaOfaTriangle() {
+        assertEquals(2, geometry.areaOfaTriangle(2, 2));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTriangle(1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTriangle(-1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTriangle(-1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTriangle(0, 0));
     }
 
     @Test
     void perimeterOfaTriangle() {
+        assertEquals(6, geometry.perimeterOfaTriangle(2, 2, 2));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTriangle(1, 1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTriangle(1, -1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTriangle(-1, 1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTriangle(0, 0, 0));
     }
 
     @Test
     void areaOfaCircle() {
+        assertEquals(Math.PI, geometry.areaOfaCircle(1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaCircle(-1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaCircle(0));
     }
 
     @Test
     void perimeterOfaCircle() {
+        assertEquals(2*Math.PI, geometry.perimeterOfaCircle(1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaCircle(-1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaCircle(0));
     }
 
     @Test
     void areaOfaTrapeze() {
+        assertEquals(2, geometry.areaOfaTrapeze(2, 2, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTrapeze(1, 1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTrapeze(1, -1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTrapeze(-1, 1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaTrapeze(0, 0, 0));
     }
 
     @Test
     void perimeterOfaTrapeze() {
+        assertEquals(8, geometry.perimeterOfaTrapeze(2, 2, 2, 2));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTrapeze(1, 1, 1, -1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTrapeze(1, 1, -1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTrapeze(1, -1, 1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTrapeze(-1, 1, 1, 1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaTrapeze(0, 0, 0, 0));
+
     }
 
     @Test
     void areaOfaSquare() {
+        assertEquals(4, geometry.areaOfaSquare(2));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaSquare(-1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.areaOfaSquare(0));
     }
 
     @Test
     void perimeterOfaSquare() {
+        assertEquals(4, geometry.perimeterOfaSquare(1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaSquare(-1));
+        assertThrows(IllegalArgumentException.class, () -> geometry.perimeterOfaSquare(0));
     }
 }
